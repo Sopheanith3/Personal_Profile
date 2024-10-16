7caf8d2508be5f5c05a16716e8794d073c85c886
Sopheanith3 <nsopheanith3@gmail.com>
2024-10-15 21:36:06 -0700

    Update the resume, project and about me button and style them better than last time.

--- ProfileWeb/script.js
+++ ProfileWeb/script.js
@@ -1,7 +1,16 @@
 // Get references to the DOM elements
 const resumeButton = document.getElementById('resumeButton');
-const pdfSlideshow = document.getElementById('pdfslideshow');
+const pdfSlideshow = document.getElementById('imgslideshow');
 const closeBtn = document.getElementById('closeBtn');
+
+const aboutMeButton = document.getElementById('aboutMeButton');
+const aboutMeModal = document.getElementById('aboutMeModal');
+const closeAboutMeModal = document.getElementById('closeAboutMeModal');
+
+const projectButton = document.getElementById('projectButton');
+const myProject = document.getElementById('project');
+const closeProject = document.getElementById('closeProject');
+
 
 // Event listener for the resume button
 resumeButton.addEventListener('click', function() {
@@ -10,10 +19,30 @@ resumeButton.addEventListener('click', function() {
 
 // Event listener for the close button
 closeBtn.addEventListener('click', function() {
-    console.log('Close button clicked');
     pdfSlideshow.style.display = 'none'; // Hide the slideshow
 });
 
+// About Me button listener
+aboutMeButton.addEventListener('click', function() {
+    aboutMeModal.style.display = 'flex'; // Show About Me modal
+});
+
+// Close About Me modal
+closeAboutMeModal.addEventListener('click', function() {
+    aboutMeModal.style.display = 'none'; // Hide the modal
+});
+
+// Show Project modal
+projectButton.addEventListener('click', function() {
+    myProject.style.display = 'flex'; 
+});
+
+// Close Project modal
+closeProject.addEventListener('click', function() {
+    myProject.style.display = 'none'; // Hide the modal when the close button is clicked
+});
+
+// LinkedIn and GitHub button listeners
 document.getElementById("linkedinButton").addEventListener("click", function() {
     window.open("https://www.linkedin.com/in/sopheanith-ny3/", "_blank");
 });
@@ -21,4 +50,3 @@ document.getElementById("linkedinButton").addEventListener("click", function() {
 document.getElementById("githubButton").addEventListener("click", function() {
     window.open("https://github.com/Sopheanith3", "_blank");
 });
-
